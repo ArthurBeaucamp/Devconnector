3e80659e5d49e64c9e47f33fb37e34595999f0ca
Arthur Beaucamp <arthur_beaucamp@laposte.net>
2018-12-13 18:00:54 +0100

    JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://Arthur:nmzgURnPJ3yaE2T@ds119374.mlab.com:19374/beaucampdevconnector'
+  mongoURI: 'mongodb://Arthur:nmzgURnPJ3yaE2T@ds119374.mlab.com:19374/beaucampdevconnector',
+  secretOrKey: 'secret'
 };
