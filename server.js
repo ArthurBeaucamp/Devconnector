658791a04ed0286cc01534fd08c41fb256f7fee4
Arthur Beaucamp <arthur_beaucamp@laposte.net>
2018-12-13 17:30:38 +0100

    User Registration

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const posts = require('./route/api/posts');
 const profile = require('./route/api/profile');
@@ -7,12 +8,16 @@ const users = require('./route/api/users');
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB Config
 const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db)
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
